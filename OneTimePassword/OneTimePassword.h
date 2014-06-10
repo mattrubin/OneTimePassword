39c0f55d404bf9e058def6808c6566fe6a3cb3d2
Matt Rubin <opensource@mattrubin.me>
2014-06-10 03:02:54 -0400

    Initial Commit

---
+++ OneTimePassword/OneTimePassword.h
@@ -0,0 +1,19 @@
+//
+//  OneTimePassword.h
+//  OneTimePassword
+//
+//  Created by Matt Rubin on 6/10/14.
+//  Copyright (c) 2014 Matt Rubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OneTimePassword.
+FOUNDATION_EXPORT double OneTimePasswordVersionNumber;
+
+//! Project version string for OneTimePassword.
+FOUNDATION_EXPORT const unsigned char OneTimePasswordVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OneTimePassword/PublicHeader.h>
+
+
