946f0daf59ac34412bde7d5e570ee8192372b2d0
Matt Rubin <opensource@mattrubin.me>
2014-07-08 11:28:56 -0400

    Stub Token-Generation.swift

---
+++ OneTimePassword/Token+Generation.swift
@@ -0,0 +1,13 @@
+//
+//  Token+Generation.swift
+//  OneTimePassword
+//
+//  Created by Matt Rubin on 7/8/14.
+//  Copyright (c) 2014 Matt Rubin. All rights reserved.
+//
+
+import Foundation
+
+extension Token {
+
+}
