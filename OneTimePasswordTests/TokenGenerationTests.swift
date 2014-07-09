f6a275b6e0b9767dca7a4300c0bca9dd092aaa5c
Matt Rubin <opensource@mattrubin.me>
2014-07-09 03:40:08 -0400

    Convert TokenGenerationTests to Swift

--- OneTimePasswordTests/TokenGenerationTests.swift
+++ OneTimePasswordTests/TokenGenerationTests.swift
@@ -7,111 +7,113 @@
 //
 
 import XCTest
+import OneTimePassword
 
 class TokenGenerationTests: XCTestCase {
 
-/*
     // The values in this test are found in Appendix D of the HOTP RFC
     // https://tools.ietf.org/html/rfc4226#appendix-D
-    - (void)testHOTPRFCValues
-    {
-        NSData *secret = [@"12345678901234567890" dataUsingEncoding:NSASCIIStringEncoding];
-        OTPToken *token = [[OTPToken alloc] init];
-        token.type = OTPTokenTypeCounter;
-        token.secret = secret;
-        token.algorithm = OTPAlgorithmSHA1;
-        token.digits = 6;
-        token.counter = 0;
+    func testHOTPRFCValues() {
+        let secret = "12345678901234567890".dataUsingEncoding(NSASCIIStringEncoding)
+        let token = OTPToken()
+        token.type = .Counter
+        token.secret = secret
+        token.algorithm = .SHA1
+        token.digits = 6
+        token.counter = 0
 
-        XCTAssertEqualObjects(@"755224", [token generatePasswordForCounter:0], @"The 0th OTP should be the expected string.");
-        XCTAssertEqualObjects(@"755224", [token generatePasswordForCounter:0], @"The generatePasswordForCounter: method should be idempotent.");
+        XCTAssertEqualObjects("755224", token.generatePasswordForCounter(0), "The 0th OTP should be the expected string.")
+        XCTAssertEqualObjects("755224", token.generatePasswordForCounter(0), "The generatePasswordForCounter: method should be idempotent.")
 
-        NSArray *expectedValues = @[@"287082",
-                                    @"359152",
-                                    @"969429",
-                                    @"338314",
-                                    @"254676",
-                                    @"287922",
-                                    @"162583",
-                                    @"399871",
-                                    @"520489"];
+        let expectedValues = [
+            "287082",
+            "359152",
+            "969429",
+            "338314",
+            "254676",
+            "287922",
+            "162583",
+            "399871",
+            "520489",
+        ]
 
-        for (NSString *expectedPassword in expectedValues) {
-            [token updatePassword];
-            XCTAssertEqualObjects(token.password, expectedPassword, @"The generator did not produce the expected OTP.");
+        for expectedPassword: String in expectedValues {
+            token.updatePassword()
+            XCTAssertEqualObjects(token.password(), expectedPassword, "The generator did not produce the expected OTP.")
         }
     }
 
     // The values in this test are found in Appendix B of the TOTP RFC
     // https://tools.ietf.org/html/rfc6238#appendix-B
-    - (void)testTOTPRFCValues
-    {
-        NSDictionary *secretKeys = @{kOTPAlgorithmSHA1:   @"12345678901234567890",
-                                     kOTPAlgorithmSHA256: @"12345678901234567890123456789012",
-                                     kOTPAlgorithmSHA512: @"1234567890123456789012345678901234567890123456789012345678901234"};
+    func testTOTPRFCValues() {
+        let secretKeys = [
+            OTPAlgorithm.SHA1:   "12345678901234567890",
+            OTPAlgorithm.SHA256: "12345678901234567890123456789012",
+            OTPAlgorithm.SHA512: "1234567890123456789012345678901234567890123456789012345678901234",
+        ]
 
-        NSArray *times = @[@59,
-                           @1111111109,
-                           @1111111111,
-                           @1234567890,
-                           @2000000000,
-                           @20000000000];
+        let times: Array<NSTimeInterval> = [
+            59,
+            1111111109,
+            1111111111,
+            1234567890,
+            2000000000,
+            20000000000,
+        ]
 
-        NSDictionary *expectedValues = @{kOTPAlgorithmSHA1:   @[@"94287082", @"07081804", @"14050471", @"89005924", @"69279037", @"65353130"],
-                                         kOTPAlgorithmSHA256: @[@"46119246", @"68084774", @"67062674", @"91819424", @"90698825", @"77737706"],
-                                         kOTPAlgorithmSHA512: @[@"90693936", @"25091201", @"99943326", @"93441116", @"38618901", @"47863826"]};
+        let expectedValues = [
+            OTPAlgorithm.SHA1:   ["94287082", "07081804", "14050471", "89005924", "69279037", "65353130"],
+            OTPAlgorithm.SHA256: ["46119246", "68084774", "67062674", "91819424", "90698825", "77737706"],
+            OTPAlgorithm.SHA512: ["90693936", "25091201", "99943326", "93441116", "38618901", "47863826"],
+        ]
 
-        for (NSString *algorithmKey in secretKeys) {
-            NSData *secret = [secretKeys[algorithmKey] dataUsingEncoding:NSASCIIStringEncoding];
-            OTPToken *token = [[OTPToken alloc] init];
-            token.type = OTPTokenTypeTimer;
-            token.secret = secret;
-            token.algorithm = [algorithmKey algorithmValue];
-            token.digits = 8;
-            token.period = 30;
+        for (algorithm, secretKey) in secretKeys {
+            let secret = secretKey.dataUsingEncoding(NSASCIIStringEncoding)
+            let token = OTPToken()
+            token.type = .Timer
+            token.secret = secret
+            token.algorithm = algorithm
+            token.digits = 8
+            token.period = 30
 
-            for (NSUInteger i = 0; i < times.count; i++) {
-                NSString *password = expectedValues[algorithmKey][i];
-                token.counter = (uint64_t)([times[i] doubleValue] / token.period);
-                XCTAssertEqualObjects([token generatePasswordForCounter:token.counter], password, @"The generator did not produce the expected OTP.");
+            for (var i = 0; i < times.count; i++) {
+                if let password = expectedValues[algorithm]?[i] {
+                    token.counter = UInt64(times[i] / token.period)
+                    XCTAssertEqualObjects(token.generatePasswordForCounter(token.counter), password, "The generator did not produce the expected OTP.")
+                }
             }
         }
     }
 
     // From Google Authenticator for iOS
     // https://code.google.com/p/google-authenticator/source/browse/mobile/ios/Classes/TOTPGeneratorTest.m
-    - (void)testTOTPGoogleValues
-    {
-        NSData *secret = [@"12345678901234567890" dataUsingEncoding:NSASCIIStringEncoding];
+    func testTOTPGoogleValues() {
+        let secret = "12345678901234567890".dataUsingEncoding(NSASCIIStringEncoding)
+        let intervals: Array<NSTimeInterval> = [1111111111, 1234567890, 2000000000]
+        let algorithms = [OTPAlgorithm.SHA1, OTPAlgorithm.SHA256, OTPAlgorithm.SHA512]
 
-        NSTimeInterval intervals[] = { 1111111111, 1234567890, 2000000000 };
+        let results = [
+            // SHA1    SHA256    SHA512
+            "050471", "584430", "380122", // date1
+            "005924", "829826", "671578", // date2
+            "279037", "428693", "464532", // date3
+        ]
 
-        NSArray *algorithms = @[kOTPAlgorithmSHA1,
-                                kOTPAlgorithmSHA256,
-                                kOTPAlgorithmSHA512,
-                                ];
-        NSArray *results = @[// SHA1      SHA256     SHA512
-                             @"050471", @"584430", @"380122", // date1
-                             @"005924", @"829826", @"671578", // date2
-                             @"279037", @"428693", @"464532", // date3
-                             ];
-
-        for (unsigned int i = 0, j = 0; i < sizeof(intervals)/sizeof(*intervals); i++) {
-            for (NSString *algorithmKey in algorithms) {
-                OTPToken *token = [[OTPToken alloc] init];
-                token.type = OTPTokenTypeTimer;
-                token.secret = secret;
-                token.algorithm = [algorithmKey algorithmValue];
-                token.digits = 6;
-                token.period = 30;
-                token.counter = (uint64_t)(intervals[i] / token.period);
+        for (var i = 0, j = 0; i < intervals.count; i++) {
+            for algorithm in algorithms {
+                let token = OTPToken()
+                token.type = .Timer
+                token.secret = secret
+                token.algorithm = algorithm
+                token.digits = 6
+                token.period = 30
+                token.counter = UInt64(intervals[i] / token.period)
                 
-                XCTAssertEqualObjects([results objectAtIndex:j],
-                                      [token generatePasswordForCounter:token.counter],
-                                      @"Invalid result %d, %@, %f", i, algorithmKey, intervals[i]);
-                j++;
+                XCTAssertEqualObjects(results[j],
+                                      token.generatePasswordForCounter(token.counter),
+                                      "Invalid result \(i), \(algorithm), \(intervals[i])")
+                j++
             }
         }
     }
-*/
 }
