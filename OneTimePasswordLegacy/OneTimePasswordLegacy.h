a380a42a3b1ffca2b52dff7d574831f9d5fb3812
Matt Rubin <opensource@mattrubin.me>
2014-07-10 17:00:09 -0400

    Add OneTimePasswordLegacy.framework

---
+++ OneTimePasswordLegacy/OneTimePasswordLegacy.h
@@ -0,0 +1,19 @@
+//
+//  OneTimePasswordLegacy.h
+//  OneTimePasswordLegacy
+//
+//  Created by Matt Rubin on 7/10/14.
+//  Copyright (c) 2014 Matt Rubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OneTimePasswordLegacy.
+FOUNDATION_EXPORT double OneTimePasswordLegacyVersionNumber;
+
+//! Project version string for OneTimePasswordLegacy.
+FOUNDATION_EXPORT const unsigned char OneTimePasswordLegacyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OneTimePasswordLegacy/PublicHeader.h>
+
+
