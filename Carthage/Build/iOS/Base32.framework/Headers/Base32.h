bc7ead09246931850d8b59b0323e60eb474104b8
Matt Rubin <opensource@mattrubin.me>
2015-01-25 23:19:22 -0800

    Include dependency build in git

---
+++ Carthage/Build/iOS/Base32.framework/Headers/Base32.h
@@ -0,0 +1,19 @@
+//
+//  Base32.h
+//  Base32
+//
+//  Created by Matt Rubin on 1/25/15.
+//  Public Domain
+//
+
+@import Foundation;
+
+//! Project version number for Base32.
+FOUNDATION_EXPORT double Base32VersionNumber;
+
+//! Project version string for Base32.
+FOUNDATION_EXPORT const unsigned char Base32VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Base32/PublicHeader.h>
+
+#import "MF_Base32Additions.h"
