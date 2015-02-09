8708d367520082a0b7f13dcf9284a65cdd7738cd
Matt Rubin <opensource@mattrubin.me>
2015-02-09 18:54:17 -0500

    as → as!

--- OneTimePassword/Keychain.swift
+++ OneTimePassword/Keychain.swift
@@ -9,13 +9,13 @@
 import Foundation
 
 func addKeychainItemWithAttributes(attributes: NSDictionary) -> NSData? {
-    let mutableAttributes = attributes.mutableCopy() as NSMutableDictionary
-    mutableAttributes[kSecClass as NSCopying] = kSecClassGenericPassword
-    mutableAttributes[kSecReturnPersistentRef as NSCopying] = kCFBooleanTrue
+    let mutableAttributes = attributes.mutableCopy() as! NSMutableDictionary
+    mutableAttributes[kSecClass as! NSCopying] = kSecClassGenericPassword
+    mutableAttributes[kSecReturnPersistentRef as! NSCopying] = kCFBooleanTrue
     // Set a random string for the account name.
     // We never query by or display this value, but the keychain requires it to be unique.
-    if (mutableAttributes[kSecAttrAccount as NSCopying] == nil) {
-        mutableAttributes[kSecAttrAccount as NSCopying] = NSUUID().UUIDString
+    if (mutableAttributes[kSecAttrAccount as! NSCopying] == nil) {
+        mutableAttributes[kSecAttrAccount as! NSCopying] = NSUUID().UUIDString
     }
 
     var result: Unmanaged<AnyObject>?
@@ -31,8 +31,8 @@ func addKeychainItemWithAttributes(attributes: NSDictionary) -> NSData? {
 
 func updateKeychainItemForPersistentRefWithAttributes(persistentRef: NSData, attributesToUpdate: NSDictionary) -> Bool {
     let queryDict = [
-        kSecClass as NSCopying: kSecClassGenericPassword,
-        kSecValuePersistentRef as NSCopying: persistentRef,
+        kSecClass as! NSCopying: kSecClassGenericPassword,
+        kSecValuePersistentRef as! NSCopying: persistentRef,
     ]
 
     let resultCode = SecItemUpdate(queryDict, attributesToUpdate)
@@ -41,8 +41,8 @@ func updateKeychainItemForPersistentRefWithAttributes(persistentRef: NSData, att
 
 func deleteKeychainItemForPersistentRef(persistentRef: NSData) -> Bool {
     let queryDict = [
-        kSecClass as NSCopying: kSecClassGenericPassword,
-        kSecValuePersistentRef as NSCopying: persistentRef,
+        kSecClass as! NSCopying: kSecClassGenericPassword,
+        kSecValuePersistentRef as! NSCopying: persistentRef,
     ]
 
     let resultCode = SecItemDelete(queryDict)
