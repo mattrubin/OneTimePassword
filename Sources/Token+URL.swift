4778fbb21df351ec5bd0ee143e91e928cfefffb0
Matt Rubin <opensource@mattrubin.me>
2017-10-09 22:16:54 -0400

    Improve query string parsing

--- Sources/Token+URL.swift
+++ Sources/Token+URL.swift
@@ -56,6 +56,7 @@ internal enum SerializationError: Swift.Error {
 
 internal enum DeserializationError: Swift.Error {
     case invalidURLScheme
+    case duplicateQueryItem(String)
     case missingFactor
     case invalidFactor(String)
     case invalidCounterValue(String)
@@ -144,19 +145,15 @@ private func token(from url: URL, secret externalSecret: Data? = nil) throws -> 
         throw DeserializationError.invalidURLScheme
     }
 
-    var queryDictionary = Dictionary<String, String>()
-    URLComponents(url: url, resolvingAgainstBaseURL: false)?.queryItems?.forEach { item in
-        // TODO: guard against duplicate query items for the same key.
-        queryDictionary[item.name] = item.value
-    }
+    let queryItems = URLComponents(url: url, resolvingAgainstBaseURL: false)?.queryItems ?? []
 
     let factor: Generator.Factor
     switch url.host {
     case .some(kFactorCounterKey):
-        let counterValue = try queryDictionary[kQueryCounterKey].map(parseCounterValue) ?? defaultCounter
+        let counterValue = try queryItems.value(for: kQueryCounterKey).map(parseCounterValue) ?? defaultCounter
         factor = .counter(counterValue)
     case .some(kFactorTimerKey):
-        let period = try queryDictionary[kQueryPeriodKey].map(parseTimerPeriod) ?? defaultPeriod
+        let period = try queryItems.value(for: kQueryPeriodKey).map(parseTimerPeriod) ?? defaultPeriod
         factor = .timer(period: period)
     case let .some(rawValue):
         throw DeserializationError.invalidFactor(rawValue)
@@ -164,9 +161,9 @@ private func token(from url: URL, secret externalSecret: Data? = nil) throws -> 
         throw DeserializationError.missingFactor
     }
 
-    let algorithm = try queryDictionary[kQueryAlgorithmKey].map(algorithmFromString) ?? defaultAlgorithm
-    let digits = try queryDictionary[kQueryDigitsKey].map(parseDigits) ?? defaultDigits
-    guard let secret = try externalSecret ?? queryDictionary[kQuerySecretKey].map(parseSecret) else {
+    let algorithm = try queryItems.value(for: kQueryAlgorithmKey).map(algorithmFromString) ?? defaultAlgorithm
+    let digits = try queryItems.value(for: kQueryDigitsKey).map(parseDigits) ?? defaultDigits
+    guard let secret = try externalSecret ?? queryItems.value(for: kQuerySecretKey).map(parseSecret) else {
         throw DeserializationError.missingSecret
     }
     guard let generator = Generator(factor: factor, secret: secret, algorithm: algorithm, digits: digits) else {
@@ -178,7 +175,7 @@ private func token(from url: URL, secret externalSecret: Data? = nil) throws -> 
     var name = String(url.path.dropFirst())
 
     let issuer: String
-    if let issuerString = queryDictionary[kQueryIssuerKey] {
+    if let issuerString = try queryItems.value(for: kQueryIssuerKey) {
         issuer = issuerString
     } else if let separatorRange = name.range(of: ":") {
         // If there is no issuer string, try to extract one from the name
@@ -226,4 +223,16 @@ private func parseDigits(_ rawValue: String) throws -> Int {
         throw DeserializationError.invalidDigits(rawValue)
     }
     return digits
+}
+
+extension Array where Element == URLQueryItem {
+    func value(for name: String) throws -> String? {
+        let matchingQueryItems = self.filter({
+            $0.name == name
+        })
+        guard matchingQueryItems.count <= 1 else {
+            throw DeserializationError.duplicateQueryItem(name)
+        }
+        return matchingQueryItems.first?.value
+    }
 }
